99899d296b4a2e8a0591873d47d6e09fcd0efcbd
Isaykin Daniil Timofeevich <k0tk3y@ya.ru>
2025-02-21 12:40:35 +0300

    All new functions functional

--- 2sem practice/main.cpp
+++ 2sem practice/main.cpp
@@ -4,18 +4,26 @@
 
 int main()
 {
-    DoublyLinkedList lst_1 {(Product){"42 merch", 42000, bratyxi_42}, (Product){"kyrit_kruto", 12000, rnb_club}};
-    lst_1.push((Product){"Goblin", 1000000, rnb_club});
+    DoublyLinkedList lst_1 {(Product){"42 merch", 42000, bratyxi_42, 1}, (Product){"kyrit_kruto", 12000, rnb_club, 2}};
+    lst_1.push((Product){"Goblin", 1000000, rnb_club, 3});
 
-    DoublyLinkedList lst_2 {(Product){"Wok", 250, rnb_club}, (Product){"Burger", 250, bratyxi_42}};
+    std::cout << std::endl << "lst_1 init" << std::endl;
+    lst_1.traverse();
 
-    lst_2 = lst_2 + (Product){"Fries", 90, barabulka};
+    DoublyLinkedList lst_2 {(Product){"Wok", 250, rnb_club, 4}, (Product){"Burger", 250, bratyxi_42, 5}};
 
+    std::cout << std::endl << "lst_2 init" << std::endl;
     lst_2.traverse();
 
-    lst_2 = lst_2 - (Product){"Burger", 250, bratyxi_42, 4};
+    lst_1 += lst_2;
 
+    std::cout << std::endl << "lst_1 += lst_2" << std::endl;
+    lst_1.traverse();
+
+    lst_2 -= lst_1;
+
+    std::cout << std::endl << "lst_2 -= lst_1" << std::endl;
     lst_2.traverse();
-
+    
     return 0;
 }
