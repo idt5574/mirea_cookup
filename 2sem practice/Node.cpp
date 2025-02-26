21b907f0e7fdf9afb2d8e71efe203996732fa4c9
Isaykin Daniil Timofeevich <k0tk3y@ya.ru>
2025-02-26 08:54:05 +0300

    Applied rule of five for Node class and changed data field type from smart pointer to default object

--- 2sem practice/Node.cpp
+++ 2sem practice/Node.cpp
@@ -2,21 +2,43 @@
 
 using shared_node_obj = std::shared_ptr<Node>; // Объявляем новое имя для типа shared_ptr<Node>
 
-Node::Node(const Product& other) // Конструктор копирования
+// Конструктор по умолчанию
+
+Node::Node()
 {
-    data = std::make_shared<Product>(other); // Переопределённый оператор присваивания продукта (см. Product.cpp)
+    data = Product();
+}
+
+// Конструктор копирования
+
+Node::Node(const Node& other)
+{
+    data = other.data;
+}
+
+// Конструктор перемещения
+
+Node::Node(Node&& move)
+{
+    data = move.data;
+    move.data = Product();
+}
+
+Node::Node(const Product& other) // Конструктор преобразования
+{
+    data = other; // Переопределённый оператор присваивания продукта (см. Product.cpp)
 }
 
 // Следующая часть сеттеров сделана потому, что напрямую к объекту класса Product из объекта класса Node не можем
 
 void Node::set_nameplate(const char* n)
-{ data->set_name(n); }
+{ data.set_name(n); }
 
 void Node::set_price(unsigned p)
-{ data->set_price(p); }
+{ data.set_price(p); }
 
 void Node::set_supplier(_suppliers_ s)
-{ data->set_supplier(s); }
+{ data.set_supplier(s); }
 
 // Конец вспомогательных сеттеров
 
@@ -29,16 +51,16 @@ void Node::set_prev(std::shared_ptr<Node> n) // Сеттер для предыд
 // Далее идут вспомогательные геттеры, сделанные потому, что напрямую к объекту класса Product из объекта класса Node не можем
 
 const unsigned& Node::get_id()
-{ return data->get_id(); }
+{ return data.get_id(); }
 
 const std::string& Node::get_name()
-{ return data->get_name(); }
+{ return data.get_name(); }
 
 const unsigned& Node::get_price()
-{ return data->get_price(); }
+{ return data.get_price(); }
 
 const _suppliers_& Node::get_supplier()
-{ return data->get_supplier(); }
+{ return data.get_supplier(); }
 
 // Конец вспомогательных геттеров
 
@@ -48,10 +70,20 @@ shared_node_obj Node::get_prev() // Геттер предыдущего объе
 shared_node_obj Node::get_next() // Геттер следующего объекта
 { return next; }
 
+const Node& Node::operator=(const Node& other)
+{
+    data = other.data;
+}
+
+const Node& Node::operator=(Node&& move)
+{
+    data = move.data;
+    move.data = Product();
+}
+
 Node::~Node() // Деструктор
 {
     // reset - особый метод для умных указателей, позволяющий отвязать текущий объект от привязанного к нему в даннный момент указателя
-    data.reset();
     prev.reset();
     next.reset();
 }
--- 2sem practice/Node.h
+++ 2sem practice/Node.h
@@ -9,7 +9,7 @@
 
 class Node
 {
-    shared_product_obj data; // Умный указатель (см. Product.h)
+    Product data; 
 
     std::shared_ptr<Node> prev {nullptr}; // Умный указатель на предыдущую ноду
     std::shared_ptr<Node> next {nullptr}; // Умный указатель на следующую ноду
@@ -17,6 +17,10 @@ class Node
 public:
 
     // Constructors
+
+    Node();
+    Node(const Node&);
+    Node(Node&&);
 
     Node(const Product&);
 
@@ -40,6 +44,11 @@ public:
     std::shared_ptr<Node> get_next();
 
     // Other methods
+
+    // Переопределённые операции
+
+    const Node& operator=(const Node&);
+    const Node& operator=(Node&&);
 
     // Destructor
 
