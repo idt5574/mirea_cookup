f2085854ffc2088d02f1bd0724d9919c8a74a0bd
idt5574 <k0tk3y@ya.ru>
2025-05-25 13:01:49 +0300

    Added custom exceptions for storage zones

---
+++ 2sem practice/noname/ex3_headers/CustomExceptions.hpp
@@ -0,0 +1,66 @@
+#ifndef _CUSTOM_EXCEPTIONS_EX3_HPP_
+#define _CUSTOM_EXCEPTIONS_EX3_HPP_
+
+#include <exception>
+#include <string>
+
+class StorageZoneException : std::exception
+{
+protected:
+
+    std::string msg;
+
+public:
+
+    StorageZoneException() 
+    {
+        msg = "Error: unknown storage error";
+    }
+
+    virtual const char * what() const noexcept
+    { return msg.c_str(); }
+    
+    ~StorageZoneException() {}
+};
+
+class ZonesNotSettedException : public StorageZoneException
+{
+public:
+
+    ZonesNotSettedException() 
+    {
+        msg = "Error: to use storage zones features, previously you need to set storage zones\n howto: SetStorageZones(const std::vector<std::string, 5>*)";
+    }
+
+    
+    ~ZonesNotSettedException() {}
+
+};
+
+class ZonesAlreadySettedException : public StorageZoneException
+{
+public:
+
+    ZonesAlreadySettedException()
+    {
+        msg = "Error: Storage zones is already setted, you can't set storage zones twice!\n";
+    }
+
+    ~ZonesAlreadySettedException() {}
+
+};
+
+class UnknownStorageZoneException : public StorageZoneException
+{
+public:
+
+    UnknownStorageZoneException()
+    {
+        msg = "Error: Unknown storage zone name setted. Use storage zone names from setted storage zones array";
+    }
+
+    ~UnknownStorageZoneException() {}
+
+};
+
+#endif 
