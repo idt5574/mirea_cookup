b3b25aaf9cd99300e7acf353acd1b4538debffc1
idt5574 <k0tk3y@ya.ru>
2025-05-28 09:51:18 +0300

    Added sketch of realisation of genre storage and storage zones inside Library class

--- 2sem practice/noname/ex3_headers/Library.hpp
+++ 2sem practice/noname/ex3_headers/Library.hpp
@@ -6,30 +6,47 @@
 
 #include "Book.hpp"
 
+#include "StorageZones.hpp"
+#include "GenreStorage.hpp"
+
 #include "custom_exceptions/LibraryExceptions.hpp"
+
+/*
+Надо написать:
+
+логику для взаимодействия библиотеки и хранилища жанров 
+    - При добавлении книги, добавить её жанр в хранилище
+
+логику для взаимодействия библитеки и хранилища зон:
+    - При добавлении книги, проверка, есть ли в библиотеке зона для неё, если нет, то изменить зону книги на null
+    - При изменении названия зоны, зоны всех книг текущей библиотеки меняются либо на null либо на название этой зоны (задать параметром bool в функции)
+    - Возможно стоит сделать класс StorageZones приватным подклассом Library
+*/
 
 class Library
 {
     std::map<std::string, Book> library;
-    bool isLibrarySetted;
+
+    GenreStorage genres;
+    StorageZones zones;
 
 public:
 
-    Library() : library(), isLibrarySetted(false) {}
+    Library(const StorageZones& newStorageZones) : library(), zones(newStorageZones) {}
+
+    GenreStorage& GetGenreStorage() 
+    { return genres; }
+
+    StorageZones& GetStorageZones()
+    { return zones; }
 
     void AddBook(const Book& newBook)
     {
-        if(!isLibrarySetted)
-            throw LibraryIsNotSettedException();
-
         library.insert({newBook.GetTitle(), newBook});
     }
 
     void RemoveBook(const std::string& bookTitle)
     {
-        if(!isLibrarySetted)
-            throw LibraryIsNotSettedException();
-
         try
         {
             library.at(bookTitle);
@@ -45,9 +62,6 @@ public:
 
     void RemoveBook(const Book& book)
     {
-        if(!isLibrarySetted)
-            throw LibraryIsNotSettedException();
-
         try
         {
             library.at(book.GetTitle());
@@ -63,7 +77,48 @@ public:
 
     const Book& TakeBook(const std::string& bookTitle)
     {
-         
+        Book* currentBook;
+
+        try
+        {
+            currentBook = &library.at(bookTitle);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Warning: You tried to \"take\" a non-existent element from library. The process will continue" << '\n';
+            return Book();
+        } 
+
+        if(currentBook->GetAvailability())
+            return *currentBook;
+        else
+        {
+            std::cout << "Warning: You tried to \"take\" a non-available book. The process will continue" << '\n';
+            return Book();
+        }
+    }
+
+    const Book& TakeBook(const Book& book)
+    {
+        Book* currentBook;
+
+        try
+        {
+            currentBook = &library.at(book.GetTitle());
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << "Warning: You tried to \"take\" a non-existent element from library. The process will continue" << '\n';
+            return Book();
+        } 
+
+        if(currentBook->GetAvailability())
+            return *currentBook;
+        else
+        {
+            std::cout << "Warning: You tried to \"take\" a non-available book. The process will continue" << '\n';
+            return Book();
+        }
     }
 
 };
